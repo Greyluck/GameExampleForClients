359bfc02f8f59dc0c10096dbd66107a4eda6257e
Greyluck <36477858+Greyluck@users.noreply.github.com>
2022-12-19 01:34:14 -0300

    PowerUp Added

--- My project (1)/Assets/Scripts/LevelControl/ObjectGenerator.cs
+++ My project (1)/Assets/Scripts/LevelControl/ObjectGenerator.cs
@@ -8,22 +8,59 @@ public class ObjectGenerator : MonoBehaviour
     public Asteroid asteroidPrefab;
     private float asteroidDelay;
     
+    public PowerMod powerUpPrefab1_Clean;
+    public PowerMod powerUpPrefab2_Shield;
+    public PowerMod powerUpPrefab3_Life;
+    private PowerMod theChosenPrefab;
+
+    
+    private float powerUpDelay;
+
     // Start 
     void Start(){
         //This will start a loop that creates the asteroids.
-        Invoke(nameof(AsteroidGeneratorMethod), 2);         
+        Invoke(nameof(AsteroidGeneratorMethod), 2);   
+        Invoke(nameof( PowerUpGeneratorMethod), 1);   
     }
 
     private void AsteroidGeneratorMethod(){
         // Randomized the X position of the asteroid and then used it to determines where it will be created.
-        float asteroidRandomXPosition = Random.Range(-9.0f, 9.0f); 
-        Vector3 asteroidCreationPosition = new Vector3 (asteroidRandomXPosition, this.transform.position.y, 0);
+        float randomXPosition = Random.Range(-9.0f, 9.0f); 
+        Vector3 finalCreationPosition = new Vector3 (randomXPosition, this.transform.position.y, 0);
     
         // Create the asteroid, set a new random wait for the nextone and start again the loop.
-        Instantiate (this.asteroidPrefab, asteroidCreationPosition, Quaternion.identity);
+        Instantiate (this.asteroidPrefab, finalCreationPosition, Quaternion.identity);
         asteroidDelay = Random.Range(2.0f, 9.0f);
         Invoke(nameof(AsteroidGeneratorMethod), asteroidDelay);
-        Debug.Log("Waiting " + asteroidDelay + "seconds until new asteroid");
+        Debug.Log("Waiting " + asteroidDelay + " seconds until new asteroid");
     }
+
+    private void PowerUpGeneratorMethod(){
+        // Randomized the type of power up that will be send
+        int theChosenValue = Random.Range(1,4);
+        switch (theChosenValue){
+        case 1:
+            theChosenPrefab = powerUpPrefab1_Clean;
+            break;
+        case 2:
+            theChosenPrefab = powerUpPrefab2_Shield;
+            break;
+        case 3:
+            theChosenPrefab = powerUpPrefab3_Life;
+            break;
+        }
+
+        // Randomized the X position of the power up and then used it to determines where it will be created.
+        float randomXPosition = Random.Range(-9.0f, 9.0f); 
+        Vector3 finalCreationPosition = new Vector3 (randomXPosition, this.transform.position.y, 0);
+    
+        // Create the power up, set a new random wait for the nextone and start again the loop.
+        Instantiate (this.theChosenPrefab, finalCreationPosition, Quaternion.identity);
+        powerUpDelay = Random.Range(1.0f, 2.0f);
+        Invoke(nameof(PowerUpGeneratorMethod), powerUpDelay);
+        Debug.Log("Waiting " + powerUpDelay + " seconds until new PowerUp");
+    }
+
+    
 
 }
--- My project (1)/Assets/Scripts/LevelControl/PowerMod.cs
+++ My project (1)/Assets/Scripts/LevelControl/PowerMod.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUp : MonoBehaviour
+public class PowerMod : MonoBehaviour
 {
     // Variables
     private Vector3 direction = Vector2.down;
-    private float speed = 1;
+    private float speed = 4;
+
+    //Type of Power Up/Down
+    public int typeOfMod;
+    /*
+    --------- UP ---------
+    1 Limpiador (UP):
+    2 Invencibilidad (Up)
+    3 Curador (UP)
+    --------- DOWN ---------
+    4 Descontrol (Down)
+    5 Aniquilador (Down)
+    */
     
     // Update (Always go down)
     void Update(){
