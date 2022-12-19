72c6f7a4f13e797dc61785bff5e1324887415ea9
Greyluck <36477858+Greyluck@users.noreply.github.com>
2022-12-19 06:17:56 -0300

    Global Manager added to manipulate life and death

--- My project (1)/Assets/Scripts/GlobalControl/GlobalManager.cs
+++ My project (1)/Assets/Scripts/GlobalControl/GlobalManager.cs
@@ -3,21 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreManager : MonoBehaviour {
+public class GlobalManager : MonoBehaviour {
 
-
+	// It creates the public value of the score and the text to be used
 	public static int actualScore = 0;
 	private GameObject scoreText;
 
-	// Use this for initialization
-	void Start () {	
-	}
-
+	// This add value to the score.
 	public static int addScore (){
 		actualScore++;
 		Debug.Log ("se aumento el score en 1");
 		return actualScore;
-	
 	}
 
 
--- My project (1)/Assets/Scripts/LevelControl/Proyectile.cs
+++ My project (1)/Assets/Scripts/LevelControl/Proyectile.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Proyectile : MonoBehaviour
 {
@@ -8,14 +9,34 @@ public class Proyectile : MonoBehaviour
     private Vector3 direction = Vector2.up;
     private float speed = 10;
     
+    // TO EDIT
+	private GameObject scoreText;
+	public int newScore = 0;
+	public GameObject myCanvas;
+	public int winScore = 10;
+
+
+	void Start(){
+		//find the actual text where the score is showed
+		scoreText = GameObject.Find ("Txt_Score");
+		myCanvas = GameObject.Find ("Canvas");
+	}
+
     // Update (Always go up)
     void Update(){
         this.transform.position += this.direction * this.speed * Time.deltaTime;
     }
 
     // This will trigger every time the proyectile colides with any item
-    private void OnTriggerEnter2D (Collider2D other){
+    private void OnTriggerEnter2D (Collider2D collider2D){
+        if (collider2D.gameObject.tag == "Asteroid")  {
+			newScore = GlobalManager.addScore();
+			scoreText.GetComponent<Text> ().text = "Score: " + newScore;
+        }
+        if (newScore == winScore){
+            myCanvas.SendMessage("Win");
+		}
         Destroy(this.gameObject);
-        Debug.Log("Bullet Destroyed");
     }
 }
+
